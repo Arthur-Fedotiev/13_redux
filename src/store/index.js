e36d039605b2f2ffc02c31adc4a2a3c9fab5edc7
romannikiforov <escaro@ukr.net>
2020-10-30 20:08:12 +0200

    ready devToools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import {createStore} from "redux"
+import {devToolsEnhancer} from "redux-devtools-extension"
 import reducers from "../reducers"
 
-const store = createStore(reducers)
+const store = createStore(reducers, devToolsEnhancer({trace: true}))
 window.store = store
 
 export default store
