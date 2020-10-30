592368758c38829872f4d7f7c9c32e0413cde56a
romannikiforov <escaro@ukr.net>
2020-10-30 19:24:35 +0200

    create store ready

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 import {render} from "react-dom"
+import {Provider} from "react-redux"
 import "bootstrap/dist/css/bootstrap.min.css"
+import store from "./store"
 import App from "./App"
 
-render(<App />, document.getElementById("root"))
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
+)
